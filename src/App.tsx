3437d754096e38d3b59e372924d5bd37e19df0f4
Alexander R Torrijos <artorrijos@gmail.com>
2023-03-29 16:45:01 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Button from "@mui/material/Button";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+}
